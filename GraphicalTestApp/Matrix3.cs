e4627de6699fe7a9e978476ff8e014ab832c37ab
Matthew John Ragas <mjrag310@hotmail.com>
2019-12-11 17:51:50 -0600

    peer reveiw stuff

--- GraphicalTestApp/Matrix3.cs
+++ GraphicalTestApp/Matrix3.cs
@@ -6,10 +6,194 @@ using System.Threading.Tasks;
 
 namespace GraphicalTestApp
 {
+    //class Matrix3
+    //{
+    //    public float m11, m12, m13, m21, m22, m23, m31, m32, m33;
+
+    //    public Matrix3()
+    //    {
+    //        m11 = 1; m12 = 0; m13 = 0;
+    //        m21 = 0; m22 = 1; m23 = 0;
+    //        m31 = 0; m32 = 0; m33 = 1;
+    //    }
+
+    //    //Creates a Matrix3 ith the specidfied values
+    //    public Matrix3(
+    //        float m11, float m12, float m13, float m21, float m22,
+    //        float m23, float m31, float m32, float m33)
+    //    {
+    //        this.m11 = m11; this.m12 = m12; this.m13 = m13;
+    //        this.m21 = m21; this.m22 = m22; this.m23 = m23;
+    //        this.m31 = m31; this.m32 = m32; this.m33 = m33;
+    //    }
+    //    //Copy Constructor
+    //    public Matrix3(Matrix3 matrix3)
+    //    {
+    //        m11 = matrix3.m11; m12 = matrix3.m12; m13 = matrix3.m13;
+    //        m21 = matrix3.m21; m22 = matrix3.m22; m23 = matrix3.m23;
+    //        m31 = matrix3.m31; m32 = matrix3.m32; m33 = matrix3.m33;
+    //    }
+
+    //    public override string ToString()
+    //    {
+    //        return
+    //            "{[" + m11 + ", " + m12 + ", " + m13 + "]," +
+    //            "[" + m21 + ", " + m22 + ", " + m23 + "]," +
+    //            "[" + m31 + ", " + m32 + ", " + m33 + "]}";
+    //    }
+
+    //    public static Matrix3 operator *(Matrix3 lhs, Matrix3 rhs)
+    //    {
+    //        return new Matrix3(
+    //            //m11
+    //            lhs.m11 * rhs.m11 + lhs.m12 * rhs.m21 + lhs.m13 * rhs.m31,
+    //            //m12
+    //            lhs.m11 * rhs.m12 + lhs.m12 * rhs.m22 + lhs.m13 * rhs.m32,
+    //            //m13
+    //            lhs.m11 * rhs.m13 + lhs.m12 * rhs.m23 + lhs.m13 * rhs.m33,
+    //            //m21
+    //            lhs.m21 * rhs.m11 + lhs.m22 * rhs.m21 + lhs.m23 * rhs.m31,
+    //            //m22
+    //            lhs.m21 * rhs.m12 + lhs.m22 * rhs.m22 + lhs.m23 * rhs.m32,
+    //            //m23
+    //            lhs.m21 * rhs.m13 + lhs.m22 * rhs.m23 + lhs.m23 * rhs.m33,
+    //            //m31
+    //            lhs.m31 * rhs.m11 + lhs.m32 * rhs.m21 + lhs.m33 * rhs.m31,
+    //            //m32
+    //            lhs.m31 * rhs.m12 + lhs.m32 * rhs.m22 + lhs.m33 * rhs.m32,
+    //            //m33
+    //            lhs.m31 * rhs.m13 + lhs.m32 * rhs.m23 + lhs.m33 * rhs.m33);
+    //    }
+
+    //    public static Vector3 operator *(Matrix3 lhs, Vector3 rhs)
+    //    {
+    //        return new Vector3(
+    //            lhs.m11 * rhs.x + lhs.m12 * rhs.y + lhs.m13 * rhs.z,
+    //            lhs.m21 * rhs.x + lhs.m22 * rhs.y + lhs.m23 * rhs.z,
+    //            lhs.m31 * rhs.x + lhs.m32 * rhs.y + lhs.m33 * rhs.z);
+    //    }
+
+    //    public void SetScaled(Vector3 v)
+    //    {
+    //        m11 = v.x; m12 = 0; m13 = 0;
+    //        m21 = 0; m22 = v.y; m23 = 0;
+    //        m31 = 0; m32 = 0; m33 = v.z;
+    //    }
+
+    //    public void SetScaled(float x, float y, float z)
+    //    {
+    //        m11 = x; m12 = 0; m13 = 0;
+    //        m21 = 0; m22 = y; m23 = 0;
+    //        m31 = 0; m32 = 0; m33 = z;
+    //    }
+    //    //Sets this Matrix3 to the specidfied values
+    //    public void Set(
+    //       float m11, float m12, float m13, float m21, float m22,
+    //       float m23, float m31, float m32, float m33)
+    //    {
+    //        this.m11 = m11; this.m12 = m12; this.m13 = m13;
+    //        this.m21 = m21; this.m22 = m22; this.m23 = m23;
+    //        this.m31 = m31; this.m32 = m32; this.m33 = m33;
+    //    }
+
+    //    //Sets this Matrix3 to the specified values
+    //    public void Set(Matrix3 matrix3)
+    //    {
+    //        m11 = matrix3.m11; m12 = matrix3.m12; m13 = matrix3.m13;
+    //        m21 = matrix3.m21; m22 = matrix3.m22; m23 = matrix3.m23;
+    //        m31 = matrix3.m31; m32 = matrix3.m32; m33 = matrix3.m33;
+    //    }
+
+    //    public void Scale(float x, float y, float z)
+    //    {
+    //        Matrix3 m = new Matrix3();
+    //        m.SetScaled(x, y, z);
+    //        Set(this * m);
+    //    }
+
+    //    void Scale(Vector3 v)
+    //    {
+    //        Matrix3 m = new Matrix3();
+    //        m.SetScaled(v.x, v.y, v.z);
+    //        Set(this * m);
+    //    }
+
+    //    public void SetRotateX(double radians)
+    //    {
+    //        Set(1, 0, 0,
+    //            0, (float)Math.Cos(radians), (float)-Math.Sin(radians),
+    //            0, (float)Math.Sin(radians), (float)Math.Cos(radians));
+    //    }
+
+    //    public void SetRotateY(double radians)
+    //    {
+    //        Set((float)Math.Cos(radians), 0, (float)Math.Sin(radians),
+    //            0, 1, 0,
+    //            (float)-Math.Sin(radians), 0, (float)Math.Cos(radians));
+    //    }
+
+    //    public void SetRotateZ(double radians)
+    //    {
+    //        Set((float)Math.Cos(radians), (float)-Math.Sin(radians), 0,
+    //            (float)Math.Sin(radians), (float)Math.Cos(radians), 0,
+    //            0, 0, 1);
+    //    }
+
+    //    public void RotateX(double radians)
+    //    {
+    //        Matrix3 m = new Matrix3();
+    //        m.SetRotateX(radians);
+
+    //        Set(this * m);
+    //    }
+
+    //    public void RotateY(double radians)
+    //    {
+    //        Matrix3 m = new Matrix3();
+    //        m.SetRotateY(radians);
+
+    //        Set(this * m);
+    //    }
+
+    //    public void RotateZ(double radians)
+    //    {
+    //        Matrix3 m = new Matrix3();
+    //        m.SetRotateZ(radians);
+
+    //        Set(this * m);
+    //    }
+
+    //    public void SetEuler(float pitch, float yaw, float roll)
+    //    {
+    //        Matrix3 x = new Matrix3();
+    //        Matrix3 y = new Matrix3();
+    //        Matrix3 z = new Matrix3();
+    //        x.SetRotateX(pitch);
+    //        y.SetRotateY(yaw);
+    //        z.SetRotateZ(roll);
+
+    //        Set(z * y * x);
+    //    }
+
+    //    public void SetTranslation(float x, float y, float z)
+    //    {
+    //        m13 = x; m23 = y; m33 = z;
+    //    }
+
+    //    public void Translate(float x, float y, float z)
+    //    {
+    //        // apply vector offset
+    //        m13 += x; m23 += y; m33 += z;
+    //    }
+    //}
     class Matrix3
     {
+        //Static reference to the identity matrix
+        public static Matrix3 Identity = new Matrix3();
+        //Each value stored in the Matrix3
         public float m11, m12, m13, m21, m22, m23, m31, m32, m33;
 
+        //Creates a Matrix3 equal to the identity matrix
         public Matrix3()
         {
             m11 = 1; m12 = 0; m13 = 0;
@@ -17,16 +201,15 @@ namespace GraphicalTestApp
             m31 = 0; m32 = 0; m33 = 1;
         }
 
-        //Creates a Matrix3 ith the specidfied values
-        public Matrix3(
-            float m11, float m12, float m13, float m21, float m22,
-            float m23, float m31, float m32, float m33)
+        //Creates a Matrix3 with the specified values
+        public Matrix3(float m11, float m12, float m13, float m21, float m22, float m23, float m31, float m32, float m33)
         {
             this.m11 = m11; this.m12 = m12; this.m13 = m13;
             this.m21 = m21; this.m22 = m22; this.m23 = m23;
             this.m31 = m31; this.m32 = m32; this.m33 = m33;
         }
-        //Copy Constructor
+
+        //Creates a copy of the specified Matrix3
         public Matrix3(Matrix3 matrix3)
         {
             m11 = matrix3.m11; m12 = matrix3.m12; m13 = matrix3.m13;
@@ -42,61 +225,15 @@ namespace GraphicalTestApp
                 "[" + m31 + ", " + m32 + ", " + m33 + "]}";
         }
 
-        public static Matrix3 operator *(Matrix3 lhs, Matrix3 rhs)
-        {
-            return new Matrix3(
-                //m11
-                lhs.m11 * rhs.m11 + lhs.m12 * rhs.m21 + lhs.m13 * rhs.m31,
-                //m12
-                lhs.m11 * rhs.m12 + lhs.m12 * rhs.m22 + lhs.m13 * rhs.m32,
-                //m13
-                lhs.m11 * rhs.m13 + lhs.m12 * rhs.m23 + lhs.m13 * rhs.m33,
-                //m21
-                lhs.m21 * rhs.m11 + lhs.m22 * rhs.m21 + lhs.m23 * rhs.m31,
-                //m22
-                lhs.m21 * rhs.m12 + lhs.m22 * rhs.m22 + lhs.m23 * rhs.m32,
-                //m23
-                lhs.m21 * rhs.m13 + lhs.m22 * rhs.m23 + lhs.m23 * rhs.m33,
-                //m31
-                lhs.m31 * rhs.m11 + lhs.m32 * rhs.m21 + lhs.m33 * rhs.m31,
-                //m32
-                lhs.m31 * rhs.m12 + lhs.m32 * rhs.m22 + lhs.m33 * rhs.m32,
-                //m33
-                lhs.m31 * rhs.m13 + lhs.m32 * rhs.m23 + lhs.m33 * rhs.m33);
-        }
-
-        public static Vector3 operator *(Matrix3 lhs, Vector3 rhs)
-        {
-            return new Vector3(
-                lhs.m11 * rhs.x + lhs.m12 * rhs.y + lhs.m13 * rhs.z,
-                lhs.m21 * rhs.x + lhs.m22 * rhs.y + lhs.m23 * rhs.z,
-                lhs.m31 * rhs.x + lhs.m32 * rhs.y + lhs.m33 * rhs.z);
-        }
-
-        public void SetScaled(Vector3 v)
-        {
-            m11 = v.x; m12 = 0; m13 = 0;
-            m21 = 0; m22 = v.y; m23 = 0;
-            m31 = 0; m32 = 0; m33 = v.z;
-        }
-
-        public void SetScaled(float x, float y, float z)
-        {
-            m11 = x; m12 = 0; m13 = 0;
-            m21 = 0; m22 = y; m23 = 0;
-            m31 = 0; m32 = 0; m33 = z;
-        }
-        //Sets this Matrix3 to the specidfied values
-        public void Set(
-           float m11, float m12, float m13, float m21, float m22,
-           float m23, float m31, float m32, float m33)
+        //Sets this Matrix3 to the specified values
+        public void Set(float m11, float m12, float m13, float m21, float m22, float m23, float m31, float m32, float m33)
         {
             this.m11 = m11; this.m12 = m12; this.m13 = m13;
             this.m21 = m21; this.m22 = m22; this.m23 = m23;
             this.m31 = m31; this.m32 = m32; this.m33 = m33;
         }
 
-        //Sets this Matrix3 to the specified values
+        //Sets this Matrix3 to the values of the specified Matrix3
         public void Set(Matrix3 matrix3)
         {
             m11 = matrix3.m11; m12 = matrix3.m12; m13 = matrix3.m13;
@@ -104,6 +241,29 @@ namespace GraphicalTestApp
             m31 = matrix3.m31; m32 = matrix3.m32; m33 = matrix3.m33;
         }
 
+        //Returns the transpose of the Matrix3
+        public Matrix3 GetTransposed()
+        {
+            return new Matrix3(m11, m21, m31, m12, m22, m32, m13, m23, m33);
+        }
+
+        //Set this Matrix3 to a scale matrix with the specified values
+        public void SetScaled(float x, float y, float z)
+        {
+            m11 = x; m12 = 0; m13 = 0;
+            m21 = 0; m22 = y; m23 = 0;
+            m31 = 0; m32 = 0; m33 = z;
+        }
+
+        //Set this Matrix3 to a scale matrix with the specified Vector3's values
+        public void SetScaled(Vector3 v)
+        {
+            m11 = v.x; m12 = 0; m13 = 0;
+            m21 = 0; m22 = v.y; m23 = 0;
+            m31 = 0; m32 = 0; m33 = v.z;
+        }
+
+        //Scale this Matrix3 by the specified values
         public void Scale(float x, float y, float z)
         {
             Matrix3 m = new Matrix3();
@@ -111,13 +271,15 @@ namespace GraphicalTestApp
             Set(this * m);
         }
 
-        void Scale(Vector3 v)
+        //Scale this Matrix3 by the specified Vector3's values
+        public void Scale(Vector3 v)
         {
             Matrix3 m = new Matrix3();
-            m.SetScaled(v.x, v.y, v.z);
+            m.SetScaled(v);
             Set(this * m);
         }
 
+        //Set this Matrix3 to a rotate matrix with the specified values
         public void SetRotateX(double radians)
         {
             Set(1, 0, 0,
@@ -125,6 +287,15 @@ namespace GraphicalTestApp
                 0, (float)Math.Sin(radians), (float)Math.Cos(radians));
         }
 
+        //Rotate this Matrix3 with the specified values
+        public void RotateX(double radians)
+        {
+            Matrix3 m = new Matrix3();
+            m.SetRotateX(radians);
+            Set(this * m);
+        }
+
+        //Set this Matrix3 to a rotate matrix with the specified values
         public void SetRotateY(double radians)
         {
             Set((float)Math.Cos(radians), 0, (float)Math.Sin(radians),
@@ -132,6 +303,15 @@ namespace GraphicalTestApp
                 (float)-Math.Sin(radians), 0, (float)Math.Cos(radians));
         }
 
+        //Rotate this Matrix3 with the specified values
+        public void RotateY(double radians)
+        {
+            Matrix3 m = new Matrix3();
+            m.SetRotateY(radians);
+            Set(this * m);
+        }
+
+        //Set this Matrix3 to a rotate matrix with the specified values
         public void SetRotateZ(double radians)
         {
             Set((float)Math.Cos(radians), (float)-Math.Sin(radians), 0,
@@ -139,27 +319,11 @@ namespace GraphicalTestApp
                 0, 0, 1);
         }
 
-        public void RotateX(double radians)
-        {
-            Matrix3 m = new Matrix3();
-            m.SetRotateX(radians);
-
-            Set(this * m);
-        }
-
-        public void RotateY(double radians)
-        {
-            Matrix3 m = new Matrix3();
-            m.SetRotateY(radians);
-
-            Set(this * m);
-        }
-
+        //Rotate this Matrix3 with the specified values
         public void RotateZ(double radians)
         {
             Matrix3 m = new Matrix3();
             m.SetRotateZ(radians);
-
             Set(this * m);
         }
 
@@ -182,8 +346,33 @@ namespace GraphicalTestApp
 
         public void Translate(float x, float y, float z)
         {
-            // apply vector offset
             m13 += x; m23 += y; m33 += z;
+        }
+
+        //Matrix3 * Matrix3
+        public static Matrix3 operator *(Matrix3 lhs, Matrix3 rhs)
+        {
+            return new Matrix3(
+                lhs.m11 * rhs.m11 + lhs.m12 * rhs.m21 + lhs.m13 * rhs.m31,
+                lhs.m11 * rhs.m12 + lhs.m12 * rhs.m22 + lhs.m13 * rhs.m32,
+                lhs.m11 * rhs.m13 + lhs.m12 * rhs.m23 + lhs.m13 * rhs.m33,
+
+                lhs.m21 * rhs.m11 + lhs.m22 * rhs.m21 + lhs.m23 * rhs.m31,
+                lhs.m21 * rhs.m12 + lhs.m22 * rhs.m22 + lhs.m23 * rhs.m32,
+                lhs.m21 * rhs.m13 + lhs.m22 * rhs.m23 + lhs.m23 * rhs.m33,
+
+                lhs.m31 * rhs.m11 + lhs.m32 * rhs.m21 + lhs.m33 * rhs.m31,
+                lhs.m31 * rhs.m12 + lhs.m32 * rhs.m22 + lhs.m33 * rhs.m32,
+                lhs.m31 * rhs.m13 + lhs.m32 * rhs.m23 + lhs.m33 * rhs.m33);
+        }
+
+        //Matrix3 * Vector3
+        public static Vector3 operator *(Matrix3 lhs, Vector3 rhs)
+        {
+            return new Vector3(
+                lhs.m11 * rhs.x + lhs.m12 * rhs.y + lhs.m13 * rhs.z,
+                lhs.m21 * rhs.x + lhs.m22 * rhs.y + lhs.m23 * rhs.z,
+                lhs.m31 * rhs.x + lhs.m32 * rhs.y + lhs.m33 * rhs.z);
         }
     }
 
--- GraphicalTestApp/Vector4.cs
+++ GraphicalTestApp/Vector4.cs
@@ -6,6 +6,132 @@ using System.Threading.Tasks;
 
 namespace GraphicalTestApp
 {
+    //class Vector4
+
+    //{
+    //    public float x;
+    //    public float y;
+    //    public float z;
+    //    public float w;
+
+    //    public Vector4()
+    //    {
+    //        x = 0;
+    //        y = 0;
+    //        z = 0;
+    //        w = 0;
+    //    }
+    //    public Vector4(float x, float y, float z, float w)
+    //    {
+    //        this.x = x;
+    //        this.y = y;
+    //        this.z = y;
+    //        this.w = w;
+    //    }
+
+    //    //Vec3 + Vec3
+    //    public static Vector4 operator +(Vector4 lhs, Vector4 rhs)
+    //    {
+    //        return new Vector4(
+    //            lhs.x + rhs.x,
+    //            lhs.y + rhs.y,
+    //            lhs.z + rhs.z,
+    //            lhs.w + rhs.w);
+    //    }
+
+    //    //Vec3 - Vec3
+    //    public static Vector4 operator -(Vector4 lhs, Vector4 rhs)
+    //    {
+    //        return new Vector4(
+    //            lhs.x - rhs.x,
+    //            lhs.y - rhs.y,
+    //            lhs.z - rhs.z,
+    //            lhs.w - rhs.w);
+    //    }
+
+    //    //Vec3 / float
+    //    public static Vector4 operator /(Vector4 lhs, float num)
+    //    {
+    //        return new Vector4(
+    //            lhs.x / num,
+    //            lhs.y / num,
+    //            lhs.z / num,
+    //            lhs.w / num);
+    //    }
+
+    //    //float / Vec3
+    //    public static Vector4 operator /(float num, Vector4 rhs)
+    //    {
+    //        return new Vector4(
+    //            num / rhs.x,
+    //            num / rhs.y,
+    //            num / rhs.z,
+    //            num / rhs.w);
+    //    }
+
+    //    //Vec3 * float
+    //    public static Vector4 operator *(Vector4 lhs, float num)
+    //    {
+    //        return new Vector4(
+    //            lhs.x * num,
+    //            lhs.y * num,
+    //            lhs.z * num,
+    //            lhs.w * num);
+    //    }
+
+    //    //float * Vec3
+    //    public static Vector4 operator *(float num, Vector4 rhs)
+    //    {
+    //        return new Vector4(
+    //            num * rhs.x,
+    //            num * rhs.y,
+    //            num * rhs.z,
+    //            num * rhs.w);
+    //    }
+
+    //    public float Magnitude()
+    //    {
+    //        return (float)Math.Sqrt(x * x + y * y + z * z + w * w);
+    //    }
+
+    //    public float MagnitudeSqr()
+    //    {
+    //        return (x * x + y * y + z * z + w * w);
+    //    }
+
+    //    public float Distance(Vector4 other)
+    //    {
+    //        float diffX = x - other.x;
+    //        float diffY = y - other.y;
+    //        float diffZ = z - other.z;
+    //        float diffW = w - other.w;
+
+    //        return (float)Math.Sqrt(diffX * diffX + diffY * diffY + diffZ * diffZ + diffW * diffW);
+    //    }
+
+    //    public void Normalize()
+    //    {
+    //        float m = Magnitude();
+    //        x /= m;
+    //        y /= m;
+    //        z /= m;
+    //        w /= m;
+    //    }
+
+    //    public float DotProduct(Vector4 b)
+    //    {
+    //        return (x * b.x + y * b.y + z * b.z + w * b.w);
+    //    }
+
+    //    public Vector4 Cross(Vector4 rhs)
+    //    {
+    //        return new Vector4(
+    //       y * rhs.z - z * rhs.y,
+    //       z * rhs.x - x * rhs.z,
+    //       x * rhs.y - y * rhs.x,
+    //       0);
+    //    }
+    //}
     class Vector4
     {
         public float x;
@@ -13,101 +139,49 @@ namespace GraphicalTestApp
         public float z;
         public float w;
 
-        public Vector4()
+        //Creates a Vector4 with x, y, z, and w at 0
+        public Vector4() : this(0f, 0f, 0f, 0f)
         {
-            x = 0;
-            y = 0;
-            z = 0;
-            w = 0;
+
         }
+
+        //Creates a Vector4 with x, y, z, and w at the specified values
         public Vector4(float x, float y, float z, float w)
         {
             this.x = x;
             this.y = y;
-            this.z = y;
+            this.z = z;
             this.w = w;
         }
 
-        //Vec3 + Vec3
-        public static Vector4 operator +(Vector4 lhs, Vector4 rhs)
+        public override string ToString()
         {
-            return new Vector4(
-                lhs.x + rhs.x,
-                lhs.y + rhs.y,
-                lhs.z + rhs.z,
-                lhs.w + rhs.w);
+            return "{" + x + ", " + y + ", " + z + ", " + w + "}";
         }
 
-        //Vec3 - Vec3
-        public static Vector4 operator -(Vector4 lhs, Vector4 rhs)
-        {
-            return new Vector4(
-                lhs.x - rhs.x,
-                lhs.y - rhs.y,
-                lhs.z - rhs.z,
-                lhs.w - rhs.w);
-        }
-
-        //Vec3 / float
-        public static Vector4 operator /(Vector4 lhs, float num)
-        {
-            return new Vector4(
-                lhs.x / num,
-                lhs.y / num,
-                lhs.z / num,
-                lhs.w / num);
-        }
-
-        //float / Vec3
-        public static Vector4 operator /(float num, Vector4 rhs)
-        {
-            return new Vector4(
-                num / rhs.x,
-                num / rhs.y,
-                num / rhs.z,
-                num / rhs.w);
-        }
-
-        //Vec3 * float
-        public static Vector4 operator *(Vector4 lhs, float num)
-        {
-            return new Vector4(
-                lhs.x * num,
-                lhs.y * num,
-                lhs.z * num,
-                lhs.w * num);
-        }
-
-        //float * Vec3
-        public static Vector4 operator *(float num, Vector4 rhs)
-        {
-            return new Vector4(
-                num * rhs.x,
-                num * rhs.y,
-                num * rhs.z,
-                num * rhs.w);
-        }
-
+        //Returns the magnitude of the Vector4
         public float Magnitude()
         {
             return (float)Math.Sqrt(x * x + y * y + z * z + w * w);
         }
 
+        //Returns the square of the magnitude of the Vector4
         public float MagnitudeSqr()
         {
             return (x * x + y * y + z * z + w * w);
         }
 
+        //Returns the distance between this Vector4 and another
         public float Distance(Vector4 other)
         {
             float diffX = x - other.x;
             float diffY = y - other.y;
             float diffZ = z - other.z;
             float diffW = w - other.w;
-
             return (float)Math.Sqrt(diffX * diffX + diffY * diffY + diffZ * diffZ + diffW * diffW);
         }
 
+        //Normalizes this Vector4
         public void Normalize()
         {
             float m = Magnitude();
@@ -117,18 +191,86 @@ namespace GraphicalTestApp
             w /= m;
         }
 
-        public float DotProduct(Vector4 b)
+        //Returns a normalized Vector4 without modifying this one
+        public Vector4 GetNormalized()
         {
-            return (x * b.x + y * b.y + z * b.z + w * b.w);
+            return (this / Magnitude());
         }
 
-        public Vector4 Cross(Vector4 rhs)
+        //Returns the dot product of this Vector4 and another
+        public float Dot(Vector4 other)
+        {
+            return x * other.x + y * other.y + z * other.z + w * other.w;
+        }
+
+        //Returns the cross product of this Vector2 and another
+        public Vector4 Cross(Vector4 other)
         {
             return new Vector4(
-           y * rhs.z - z * rhs.y,
-           z * rhs.x - x * rhs.z,
-           x * rhs.y - y * rhs.x,
-           0);
+                y * other.z - z * other.y,
+                z * other.x - x * other.z,
+                x * other.y - y * other.x,
+                0);
+        }
+
+        //Vector4 + Vector4
+        public static Vector4 operator +(Vector4 lhs, Vector4 rhs)
+        {
+            return new Vector4(
+                lhs.x + rhs.x,
+                lhs.y + rhs.y,
+                lhs.z + rhs.z,
+                lhs.w + rhs.w);
+        }
+
+        //Vector4 - Vector4
+        public static Vector4 operator -(Vector4 lhs, Vector4 rhs)
+        {
+            return new Vector4(
+                lhs.x - rhs.x,
+                lhs.y - rhs.y,
+                lhs.z - rhs.z,
+                lhs.w - rhs.w);
+        }
+
+        //Vector4 * float
+        public static Vector4 operator *(Vector4 lhs, float rhs)
+        {
+            return new Vector4(
+                lhs.x * rhs,
+                lhs.y * rhs,
+                lhs.z * rhs,
+                lhs.w * rhs);
+        }
+
+        //float * Vector4
+        public static Vector4 operator *(float lhs, Vector4 rhs)
+        {
+            return new Vector4(
+                lhs * rhs.x,
+                lhs * rhs.y,
+                lhs * rhs.z,
+                lhs * rhs.w);
+        }
+
+        //Vector4 / float
+        public static Vector4 operator /(Vector4 lhs, float rhs)
+        {
+            return new Vector4(
+                lhs.x / rhs,
+                lhs.y / rhs,
+                lhs.z / rhs,
+                lhs.w / rhs);
+        }
+
+        //float / Vector4
+        public static Vector4 operator /(float lhs, Vector4 rhs)
+        {
+            return new Vector4(
+                lhs / rhs.x,
+                lhs / rhs.y,
+                lhs / rhs.z,
+                lhs / rhs.w);
         }
     }
 }
