1f6399d17f6ec08ecf33f6d933837751f6979a63
Matthew John Ragas <mjrag310@hotmail.com>
2019-12-12 22:39:03 -0600

    Deleted bullet classes

--- GraphicalTestApp/TankTurret.cs
+++ GraphicalTestApp/TankTurret.cs
@@ -8,32 +8,21 @@ namespace GraphicalTestApp
 {
     class TankTurret : Actor
     {
-        private Sprite _turret;
-        private InvisibleBullet _bullet;
-        private bool shot;
+        private Sprite _turret;                
 
         public TankTurret(float x, float y)
         {
             _turret = new Sprite("tankGreen_barrel2_outline.png");
-            _bullet = new InvisibleBullet(x, y);
+            
 
             X = 0;
             Y = 0;
             _turret.X = -5.5f;
             _turret.Y = 0;
 
-            AddChild(_bullet);
+            
             AddChild(_turret);
-        }
-
-        public void shoot()
-        {
-            if (Input.IsKeyPressed(32))
-            {
-                RemoveChild(_bullet);                
-                Parent.Parent.Parent.AddChild(_bullet);
-            }                                                    
-        }
+        }        
 
         public void RotateRight(float deltaTime)
         {
@@ -52,8 +41,7 @@ namespace GraphicalTestApp
         }       
 
         public override void Update(float deltaTime)
-        {
-            shoot();            
+        {            
             RotateRight(deltaTime);
             RotateLeft(deltaTime);
             base.Update(deltaTime);
